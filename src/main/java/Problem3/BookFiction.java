1a7a2224da1a2532af035350a76dcd9e29411f77
Eimi <eeimikoga@gmail.com>
2020-10-19 23:19:15 -0700

    test commit 1

--- src/main/java/Problem3/BookFiction.java
+++ src/main/java/Problem3/BookFiction.java
@@ -9,21 +9,27 @@ public class BookFiction extends Book {
     public BookFiction(String title, String author, String genres) {
         // homework
         // tip: use the 'super' keyword
+        super(title, author);
+        this.genres = genres;
     }
 
     // copy constructor
     public BookFiction(BookFiction anotherBook) {
         // homework
         // tip: use the 'super' keyword
+        super(anotherBook.title, anotherBook.author);
     }
 
     @Override
     public int getLateFeeInDollar() {
+
         return lateFeePerDayInDollar;
     }
 
     @Override
     public int calcLateFees(int numOfDaysPastDue) {
         // homework
+        int fees = lateFeePerDayInDollar * numOfDaysPastDue;
+        return fees;
     }
 }
--- src/main/java/Problem3/MovieAction.java
+++ src/main/java/Problem3/MovieAction.java
@@ -7,11 +7,13 @@ public class MovieAction extends Movie {
     public MovieAction(String rating, String title) {
         // homework
         // tip: use the 'super' keyword
+        super(rating, title);
     }
 
     public MovieAction(MovieAction anotherMovie) {
         // homework
         // tip: use the 'super' keyword
+        super(anotherMovie.rating, anotherMovie.title);
     }
 
     @Override
@@ -22,5 +24,12 @@ public class MovieAction extends Movie {
     @Override
     public int calcLateFees(int numOfDaysPastDue) {
         // homework
+        int fee = 0;
+        if(numOfDaysPastDue < 5){
+            fee = numOfDaysPastDue * lateFeePerDayInDollar;
+        }else if(numOfDaysPastDue > 5){
+            fee = 2 * numOfDaysPastDue * lateFeePerDayInDollar;
+        }
+        return fee;
     }
 }
