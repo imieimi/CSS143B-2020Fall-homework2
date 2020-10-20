b4cddc205fb401ac524f2b678049c3bdb028b2fc
Eimi <eeimikoga@gmail.com>
2020-10-20 00:51:37 -0700

    commit 3

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -3,7 +3,7 @@ public class Problem2 {
     /*
     Describe your algorithm here in up to 5 sentences:
     The loop should compare the index number to the one next to it until there are no more duplicates of the
-    number, and move the duplicates to the end and assume they are not a part of the array, then
+    number,assume they are not a part of the array, then
     update the newLength to keep count of the non duplicates.
      */
 
@@ -13,11 +13,11 @@ public class Problem2 {
         //return Integer.MAX_VALUE; // place holder
         int length = nums.length;
         int lengthMinusOne = nums.length - 1;
-        int newLength = 1;
+        int newLength = nums.length - 1;
         for(int i = 0; i < lengthMinusOne; i++){
             for(int a = 0; a < lengthMinusOne; a++){
                 if(nums[i] == nums[a]){
-                    newLength = length - 1;
+                    newLength = newLength - 1;
                 }
             }
         }
